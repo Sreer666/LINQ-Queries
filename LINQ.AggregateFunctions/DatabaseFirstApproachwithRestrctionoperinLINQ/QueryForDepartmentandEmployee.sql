cdafc47c813d5c0dccd6c65a321516b493b38a1f
Sree Ram <sreer666@gmail.com>
2019-03-23 17:20:14 -0400

    Updated Restrction Operateor and finding the index query and anysomus method

---
+++ LINQ.AggregateFunctions/DatabaseFirstApproachwithRestrctionoperinLINQ/QueryForDepartmentandEmployee.sql
@@ -0,0 +1,30 @@
+﻿Select * from Departments
+Select * from Employee
+------------------------Qurey-------------------
+create table Departments
+(
+ID int	primary key identity,
+Name Nvarchar(50),
+Location Nvarchar(50)
+)
+
+create table Employee
+(
+ID int	primary key identity,
+FirstName nvarchar(50),
+LastName nvarchar(50),
+Gender nvarchar(50),
+Salary int,
+DepartmentId int foreign key references Departments(Id)
+)
+
+insert into Departments values ('IT','Norfolk')
+insert into Departments values ('HR','ENorfolk')
+insert into Departments values ('Payroll','WNorfolk')
+
+insert into Employee values ('Mark', 'Hastrings','Male', 60000,1)
+insert into Employee values ('Mark2', 'Hastrings','Male', 70000,1)
+insert into Employee values ('Mark3', 'Hastrings','Male', 80000,1)
+insert into Employee values ('Mar4', 'Hastrings','Male', 50000,1)
+insert into Employee values ('Mark5', 'Hastrings','Male', 80000,1)
+insert into Employee values ('Mark', 'Hastrings','Female', 100000,1)
---
+++ LINQ.AggregateFunctions/DatabaseFirstApproachwithRestrctionoperinLINQ/RestrctionOperator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstApproachwithRestrctionoperinLINQ
+{
+    public class RestrctionOperator
+    {
+        static void Main(string[] args)
+        {
+            EmployeeModel employee = new EmployeeModel();
+            var departments = employee.Departments.Where(dep => dep.Name == "IT" || dep.Name=="HR");
+
+            foreach (Department dept in departments)
+            {
+                Console.WriteLine(dept.Name+"     "+dept.Location);
+
+
+                //var departmentsindex = departments.Select((dep, indexer) => new { dept1 = dep, ind = indexer });
+                //foreach (var item in departmentsindex)
+                //{
+                //    Console.WriteLine(item.ind);
+                //}
+
+
+                var emplist = dept.Employees.Where(x => x.Gender == "Male"|| x.Gender == "Female");
+                foreach (Employee emp in emplist)
+                {
+                    Console.WriteLine(emp.FirstName + "  " + emp.LastName + "   " + emp.Gender);
+                }
+                //If you want to retrive Index postion 
+
+                var empindex = emplist.Select((EmployeeModel, indexer) => new { emp1 = EmployeeModel, ind = indexer });
+                foreach(var item in empindex)
+                {
+                    Console.WriteLine(item);
+                }
+        
+
+            }
+        
+          }
+
+    }
+}
